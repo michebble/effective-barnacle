f063bd06211e04a94c37f680a8983d2e3dbd9d08
Michael Hebblethwaite <32097294+michebble@users.noreply.github.com>
2022-12-02 08:39:31 +0900

    add basic job display

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,5 +1,6 @@
 class DashboardsController < ApplicationController
   def show
     @current_admin = current_admin
+    @jobs = Job.all.includes(:client, :plumbers).order(start_on: :desc)
   end
 end
