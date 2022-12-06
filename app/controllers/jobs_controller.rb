1234a3610dfa46b0b627232c4ae254f3bcda935f
Michael Hebblethwaite <32097294+michebble@users.noreply.github.com>
2022-12-06 09:49:20 +0900

    Add job create logic

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -4,5 +4,20 @@ class JobsController < ApplicationController
   end
 
   def create
+    @job = Job.new(job_params)
+
+    if @job.valid? && @job.save
+      flash[:notice] = "Job created!"
+      redirect_to dashboard_path
+    else
+      flash.now[:alert] = @job.errors.full_messages.first
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:start_on, :client_id, plumber_ids: [])
   end
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -4,6 +4,10 @@ class Job < ApplicationRecord
 
   scope :by_period, ->(start_on_from, start_on_to) { where(start_on: start_on_from.to_date..start_on_to.to_date) }
 
+  validates :start_on, presence: true
+  validates :client, presence: true
+  validates :plumbers, presence: true
+
   def mark_as_done
     if done_at.nil?
       update(done_at: Time.current)
